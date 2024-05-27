20acbe6f3fb12f7521cab5a703a62d7fd7476fd0
pranav <pranav09j@gmail.com>
2024-05-27 12:29:27 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import Todos from './components/Todos.jsx'
+import AddTodo from './components/AddTodo.jsx'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>Learn about Redux Toolkit</h1>
+      <AddTodo />
+      <Todos />
+
+    </>
+  )
+}
+
+export default App
